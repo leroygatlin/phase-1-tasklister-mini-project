1e3f87e32b8755d3a24383e04ffdca9d53fab8f1
leroygatlin <leroygatlin@gmail.com>
2023-11-30 19:49:55 -0800

    complete tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+document.getElementById("create-task-form").addEventListener("submit",(event) => {
+event.preventDefault();
+
+const newTask = document.getElementById("new-task-description").value 
+
+const li = document.createElement("li")
+li.textContent = newTask 
+
+let x = document.createElement("button")
+x.innerHTML = "x"
+
+document.getElementById("tasks").append(li, x)
+} )
+
 });
